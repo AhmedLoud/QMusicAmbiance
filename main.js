260af7499aa21e553249c060d3de052f25eb560a
AhmedLoud <ahmed.loudghiri@gmail.com>
2018-10-02 12:06:46 -0500

    trying node-core-audio library

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log('Hello');
+// Create a new instance of node-core-audio
+var coreAudio = require('node-core-audio');
+
+// Create a new audio engine
+var engine = coreAudio.createNewAudioEngine();
+
+
+/** 
+ * Add an audio processing callback
+ * This function accepts an input buffer coming from the sound card,
+ * and returs an output buffer to be sent to your speakers.
+ * Important: This function must return an output buffer
+ **/
+// function processAudio(inputBuffer) {
+//   return inputBuffer;
+// }
+
+// engine.addAudioCallback(processAudio);
